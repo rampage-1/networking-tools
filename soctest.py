7b3ff844f4fe043ea96749179e7a325d448e71d1
rampage-1 <86386432+rampage-1@users.noreply.github.com>
2021-06-25 20:37:00 -0400

    Create soctest.py
    Sets up a short-lived TCP socket to HOST:PORT

---
+++ soctest.py
@@ -0,0 +1,18 @@
+#!/bin/python3
+# for https://academy.tcm-sec.com/p/practical-ethical-hacking-the-complete-course
+# not an original work
+
+# Sets up a short-lived TCP socket to HOST:PORT 
+
+import socket
+import sys
+
+HOST = '127.0.0.1' # loopback address for testing
+PORT = 7777 # arbitrary port
+
+# very common to this type of socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# AF_INET: IPv4, SOCK_STREAM: default TCP
+# SOC_DGRAM: UDP
+
+s.connect((HOST, PORT))
